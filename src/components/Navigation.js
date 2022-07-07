84fea6e8c9a45ef8a05c8a6c65d5b34d17861824
Florent <florent.bailly62400@gmail.com>
2022-07-07 16:47:24 +0200

    add burger menu, doesn't work yet

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,24 +3,70 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <div className='header'>
-            <h1>Florent Bailly</h1>
-            <div className='burger-menu'>
+        <div id='accueil' className='container-header-section'>
+            <header>
+                <h1>Florent Bailly</h1>
+                <div className='container-link'>
                     <NavLink to="/">
-                        <span>Accueil</span>
-                    </NavLink>
-                    <NavLink to="/a-propos">
-                        <span>À propos</span>
-                    </NavLink>
-                    <NavLink to="/portfolio">
-                        <span>Portfolio</span>
-                    </NavLink>
-                    <NavLink to="/contact">
-                        <span>Contact</span>
+                        <p>Accueil</p>
                     </NavLink>
 
+                    <NavLink to="/a-propos">
+                        <p>À propos</p>
+                    </NavLink>
+
+                    <NavLink to="/portfolio">
+                        <p>Portfolio</p>
+                    </NavLink>
+                    
+                    <NavLink to="/contact">
+                        <p>Contact</p>
+                    </NavLink>
+                </div>
+
+{/* --------------------MENU-BURGER TAB & MOBILE-------------------- */}
+
+            <div className='header--container'>
+                <div className='burger--button'>
+                    <span className='ligne'></span>
+                    <span className='ligne'></span>
+                    <span className='ligne'></span>
+                </div>
             </div>
-        </div>
+            <div className='main-menu'>
+                <nav>
+                    {/* <a className='menu--off' href="#">
+                        <p>Accueil</p>
+                    </a>
+                    <a className='menu--off' href="#">
+                        <p>A propos</p>
+                    </a>
+                    <a className='menu--off' href="#">
+                        <p>Portfolio</p>
+                    </a>
+                    <a className='menu--off' href="#">
+                        <p>Contact</p>
+                    </a> */}
+
+                    <NavLink to="/">
+                        <p>Accueil</p>
+                    </NavLink>
+
+                    <NavLink to="/a-propos">
+                        <p>À propos</p>
+                    </NavLink>
+
+                    <NavLink to="/portfolio">
+                        <p>Portfolio</p>
+                    </NavLink>
+                    
+                    <NavLink to="/contact">
+                        <p>Contact</p>
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    </div>
     );
 };
 
